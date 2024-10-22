195fae2fc73df456c3651df0cf66da5a07c0db58
Marc-Lorenz <169900493+Marc-Lorenz@users.noreply.github.com>
2024-10-22 12:03:19 +0200

    disable odering of tool menus

--- src/components/Sidebar/ToolMenuContainer.vue
+++ src/components/Sidebar/ToolMenuContainer.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { computed, defineAsyncComponent } from 'vue';
-import { Sortable } from 'sortablejs-vue3';
 import { useAnnotationToolStore } from '@/stores/annotationToolStore';
 import { AnnotationTool } from '@/enums/annotationTool';
 
@@ -25,33 +24,30 @@ function componentFromTool(tool: AnnotationTool) {
 
 <template>
   <div v-if="tools.length > 0" class="mt-1">
-    <!-- options -->
-    <Sortable :list="tools" item-key="id" tag="div">
-      <template #header> </template>
-      <template #item="{ element }">
-        <div class="draggable" :key="element.id">
-          <BAccordion free class="mt-2 bg-light rounded-1">
-            <BAccordionItem :title="element.tool" visible>
-              <component
-                v-if="componentFromTool(element.tool)"
-                :is="componentFromTool(element.tool)"
-              />
-              <div v-else>Component not found.</div>
-              <!-- remove -->
-              <hr />
-              <BButton
-                @click="annotationTools.tools.delete(element.tool)"
-                variant="outline-dark"
-                class="w-100"
-              >
-                <i class="bi bi-trash"></i>
-                Remove tool
-              </BButton>
-            </BAccordionItem>
-          </BAccordion>
-        </div>
-      </template>
-    </Sortable>
+    <!-- The list was intended to be sortable,but the faders break with this - sortablejs-vue3 -->
+    <div v-for="element in tools" :key="element.id">
+      <div class="draggable" :key="element.id">
+        <BAccordion free class="mt-2 bg-light rounded-1">
+          <BAccordionItem :title="element.tool" visible>
+            <component
+              v-if="componentFromTool(element.tool)"
+              :is="componentFromTool(element.tool)"
+            />
+            <div v-else>Component not found.</div>
+            <!-- remove -->
+            <hr />
+            <BButton
+              @click="annotationTools.tools.delete(element.tool)"
+              variant="outline-dark"
+              class="w-100"
+            >
+              <i class="bi bi-trash"></i>
+              Remove tool
+            </BButton>
+          </BAccordionItem>
+        </BAccordion>
+      </div>
+    </div>
   </div>
 </template>
 
